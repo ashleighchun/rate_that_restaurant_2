e641207b8977634814c4ee93f10ca071145f9192
Ashleigh Chun <ashleigh.chun@gmail.com>
2019-07-16 12:48:46 -0700

    generate review and restaurant resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :restaurants
+  resources :reviews
   devise_for :users, :controllers => {:registrations => "registrations"}
 
     devise_scope :user do
