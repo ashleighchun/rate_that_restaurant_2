63833e07dc917e0db9a393386a8f7d072f86b64d
Ashleigh Chun <ashleigh.chun@gmail.com>
2019-07-17 15:35:38 -0700

    refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  #devise_for :users, :controllers => {:registrations => "registrations", :omniauth_callbacks => "callbacks"}
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register', edit: 'settings' }
+  devise_for :users, :controllers => {:registrations => "registrations", :omniauth_callbacks => "callbacks"}
+
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
     get 'signup', to: 'devise/registrations#new'
