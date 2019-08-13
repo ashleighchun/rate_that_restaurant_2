ffebcecdb91f60d2da1931a14db0cbf6f28ba534
Ashleigh Chun <ashleigh.chun@gmail.com>
2019-08-12 17:19:47 -0700

    plan ajax hooks

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,17 +2,10 @@ class RestaurantsController < ApplicationController
 
   def index
     @restaurants = Restaurant.all.order_by_name
-    respond_to do |f|
-      f.html{render :index}
-      f.json {render json: @restaurants}
-    end
+    render json: @restaurants
   end
 
   def show
     @restaurant = Restaurant.find_by(id: params[:id])
-    respond_to do |f|
-      f.html {render :show}
-      f.json {render json: @post}
-    end
   end
 end
