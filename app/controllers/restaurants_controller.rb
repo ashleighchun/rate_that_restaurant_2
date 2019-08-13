11a7db271e1707a573b4af5ac446e570474f6edc
Ashleigh Chun <ashleigh.chun@gmail.com>
2019-08-12 21:53:12 -0700

    start building in js hooks to restaurant index

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,10 +2,13 @@ class RestaurantsController < ApplicationController
 
   def index
     @restaurants = Restaurant.all.order_by_name
-    render json: @restaurants
   end
 
   def show
     @restaurant = Restaurant.find_by(id: params[:id])
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @restaurant.to_json }
+    end
   end
 end
