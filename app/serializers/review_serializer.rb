82eb5d54d3dea2180a6345806e8cead42b337694
Ashleigh Chun <ashleigh.chun@gmail.com>
2019-08-12 19:07:22 -0700

    adjust routes, add serializer, add json to controller

---
+++ app/serializers/review_serializer.rb
@@ -0,0 +1,4 @@
+class ReviewSerializer < ActiveModel::Serializer
+  attributes :id, :content, :rating, :price, :title
+  belongs_to :restaurant
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,13 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => {:registrations => "registrations", :omniauth_callbacks => "callbacks"}
 
-  root to: 'application#home'
-
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
     get 'signup', to: 'devise/registrations#new'
   end
 
 
+  root to: 'application#home'
 
   resources :restaurants do #same thing as writing out get 'restaurants/:id' => "restaurants#show" for each route
     resources :reviews, only: [:new, :create, :index] #review belongs to restaurant, you want to nest url
@@ -17,8 +16,6 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show]
 
-  scope '/user' do  #get rid of nesting under user
-    resources :reviews
-  end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :reviews
+  
 end
